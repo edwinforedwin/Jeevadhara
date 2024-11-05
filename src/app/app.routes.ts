973b59c1a64f9e221d88fb0e86c034cc7d7a3664
edwinforedwin <edwinforedwin@gmail.com>
2024-11-06 00:53:37 +0530

    userhome component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { UserhomeComponent } from './components/userhome/userhome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'userhome',component:UserhomeComponent}
+];
