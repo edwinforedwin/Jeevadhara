2b7038ea6bd98093c06081ccdee535144239ee1c
edwinforedwin <edwinforedwin@gmail.com>
2024-11-07 21:54:44 +0530

    login page designed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { UserhomeComponent } from './components/userhome/userhome.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-    {path:'userhome',component:UserhomeComponent}
+    {path:'userhome',component:UserhomeComponent},
+    {path:'jeevadhara/loanportal/login',component:LoginComponent}
 ];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    InputTextModule, FormsModule, ButtonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit{
+  username: string = '';
+  securic: string = '';
+  ngOnInit() {
+    let usernameSubmitted = this.username;
+    let securicSubmitted = this.securic;
+  }
 }
