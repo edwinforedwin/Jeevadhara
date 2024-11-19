bd2fdcd5d80a5b8c5187565c4f74266110e59eac
edwinforedwin <edwinforedwin@gmail.com>
2024-11-19 13:23:59 +0530

    user group modifications

--- src/app/components/adminportal/usergroup/usergroup.component.ts
+++ src/app/components/adminportal/usergroup/usergroup.component.ts
@@ -10,23 +10,28 @@ import { FloatLabelModule } from 'primeng/floatlabel';
 import { ServiceService } from '../../../services/adminportal/service.service';
 import { map } from 'rxjs';
 import { CommonModule } from '@angular/common'
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { RippleModule } from 'primeng/ripple';
 
 interface dropLists {
   dropid: number,
   dropname: string
 }
-interface apiResponse {
-  response: string,
-  response_code: string,
-  menu_details: []
+interface apiresponse {
+  response:string,
+  err_msg:string
 }
 
 @Component({
   selector: 'app-usergroup',
   standalone: true,
-  imports: [InputTextModule,FormsModule,CardModule,DropdownModule,FieldsetModule,CheckboxModule,ButtonModule,FloatLabelModule,CommonModule],
+  imports: [
+    InputTextModule,FormsModule,CardModule,DropdownModule,FieldsetModule,CheckboxModule,ButtonModule,
+    FloatLabelModule,CommonModule,ToastModule,RippleModule],
   templateUrl: './usergroup.component.html',
-  styleUrl: './usergroup.component.css'
+  styleUrl: './usergroup.component.css',
+  providers:[MessageService]
 })
 export class UsergroupComponent {
   name: string = ''
@@ -41,14 +46,31 @@ export class UsergroupComponent {
     {dropid:2,dropname:"Loan Portal"},
     {dropid:3,dropname:"Admin Portal"}
   ]
+  apiresponse:apiresponse={response:'',err_msg:''}
 
-  //constructor(public services:ServiceService) {}
   public ugservice = inject(ServiceService)
+  public messageservice = inject(MessageService)
+
   public menus = this.ugservice.getMenus().pipe(map((resp:any)=>{
     return resp.ug_details.map((item:any)=>({
       dropid:item.menu_id,dropname:item.menu_name
     }))
   }))
+
+  createusergroup(){
+    let response = this.ugservice.createMenus(this.name,this.description,'Main Dashboard','1,2','1,2,12',1)
+    response.subscribe((resp:any)=>{
+      this.apiresponse = resp
+      if (this.apiresponse.response === 'success') {
+        this.messageservice.add({ severity: 'success', summary: this.name, detail: 'User group created successfully' })
+        this.name = ''
+        this.description = ''
+      }
+      else {
+        this.messageservice.add({ severity: 'error', summary: this.apiresponse.err_msg, detail: 'Message Content' })
+      }
+    })
+  }
   
   // getMenuList() {
   //   let apiResp = this.ugservice.getMenus()
--- src/app/services/adminportal/service.service.ts
+++ src/app/services/adminportal/service.service.ts
@@ -12,4 +12,15 @@ export class ServiceService {
   getMenus():Observable<any>{
     return this.http.get('http://localhost:234/jeevadhara/api/v1/getmenus',{headers:{'auth_value':secret.auth_value}})
   }
+  createMenus(ug_name:string,ug_desc:string,ug_dashboard:string,ug_portal:string,ug_menus:string,ug_created_by:number):Observable<any>{
+    let payload = {
+      "ug_name":ug_name,
+      "ug_desc":ug_desc,
+      "ug_dashboard":ug_dashboard,
+      "ug_portal":ug_portal,
+      "ug_menus":ug_menus,
+      "ug_created_by":ug_created_by
+  }
+    return this.http.post('http://localhost:234/jeevadhara/api/v1/createusergroup',payload,{headers:{'auth_value':secret.auth_value}})
+  }
 }
