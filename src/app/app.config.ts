4a1cd94483e3d3970bc6cc3c511ef226910e3758
edwinforedwin <edwinforedwin@gmail.com>
2024-12-15 17:58:53 +0530

    changes in config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 
